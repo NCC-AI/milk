b2320c73e2372ddfd971f16bd5809647dc5923b8
aiorhiroki <1234defgsigeru@gmail.com>
2018-11-20 10:11:33 +0900

    ncc関数のアップデート&逐次処理を可能に

--- milk/lib.py
+++ milk/lib.py
@@ -2,7 +2,7 @@ import pickle
 from sklearn import datasets, svm
 from sklearn.model_selection import train_test_split
 from glob import glob
-from ncc.models import conv3d
+from ncc.models import Model3D
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -10,9 +10,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'milk/static')
 
 
 def load_data(target_dir):
-    """訓練データセットを読み込む"""
     files = glob(target_dir+'/*/*.jpg')
-    model = conv3d(input_dim=(32, 256, 256, 3), num_classes=10)
+    model = Model3D(input_shape=(32, 256, 256, 3), num_classes=10)
     from keras.utils import plot_model
     plot_model(model, to_file=MEDIA_ROOT+'/milk/model.png')
 
@@ -20,35 +19,28 @@ def load_data(target_dir):
 
 
 def read():
-    """予測モデルを読み込む"""
     with open('mnist.pickle', 'rb') as file:
         clf = pickle.load(file)
     return clf
 
 
 def create_and_save():
-    """予測モデルを作成し、保存する"""
-    # サンプル画像データのロード
     mnist = datasets.fetch_mldata('MNIST original', data_home='image/')
     X = mnist.data / 255
     y = mnist.target
 
-    # 訓練用データとテスト用データに分ける
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, train_size=5000, test_size=0
     )
 
-    # 訓練用データで学習
     clf = svm.SVC()
     clf.fit(X_train, y_train)
 
-    # 予測モデルの保存
     with open('mnist.pickle', 'wb') as file:
         pickle.dump(clf, file)
     return clf
 
 
-# pickleで保存したデータがなければ、新しく作る
 try:
     clf = read()
 except FileNotFoundError:
@@ -56,6 +48,5 @@ except FileNotFoundError:
 
 
 def predict(img_array):
-    """手書き文字を判別した結果を返す"""
     result = clf.predict(img_array)
     return str(int(result[0]))
--- milk/views.py
+++ milk/views.py
@@ -1,11 +1,17 @@
-import base64
-from io import BytesIO
-from django.shortcuts import render
+import time
+from multiprocessing import Process
+
+from django.shortcuts import redirect, render, get_object_or_404
 from django.views import generic
+
+from .models import Progress
 from .forms import ImageUploadForm, DirectoryPathForm
 from .lib import predict, load_data
 import numpy as np
 from PIL import Image
+import os
+
+print(os.getcwd())
 
 
 class UploadView(generic.FormView):
@@ -13,16 +19,12 @@ class UploadView(generic.FormView):
     form_class = ImageUploadForm
 
     def form_valid(self, form):
-        # アップロードファイル本体を取得
         file = form.cleaned_data['file']
-        # ファイルを、28*28にリサイズし、グレースケール(モノクロ画像)
         img = Image.open(file).resize((28, 28)).convert('L')
 
-        # 学習時と同じ形に画像データを変換する
         img_array = np.asarray(img) / 255
         img_array = img_array.reshape(1, 784)
 
-        # 推論した結果を、テンプレートへ渡して表示
         context = {
             'result': predict(img_array),
         }
@@ -34,7 +36,6 @@ class TrainView(generic.FormView):
     form_class = DirectoryPathForm
 
     def form_valid(self, form):
-        # アップロードファイル本体を取得
         target_dir = form.cleaned_data['target_directory']
         files, model = load_data(target_dir)
 
@@ -46,23 +47,31 @@ class TrainView(generic.FormView):
         return render(self.request, 'milk/build_model.html', context)
 
 
-class PaintView(generic.TemplateView):
-    template_name = 'milk/paint.html'
+class HomeView(generic.CreateView):
+    """処理の開始ページ"""
+    model = Progress
+    fields = ()
+    template_name = 'milk/home.html'
 
-    def post(self, request):
-        base_64_string = request.POST['img-src'].replace(
-            'data:image/png;base64,', '')
-        file = BytesIO(base64.b64decode(base_64_string))
+    def form_valid(self, form):
+        progress_instance = form.save()
+        p = Process(target=update, args=(progress_instance.pk,), daemon=True)
+        p.start()
+        return redirect('milk:progress', pk=progress_instance.pk)
 
-        # ファイルを、28*28にリサイズし、グレースケール(モノクロ画像)
-        img = Image.open(file).resize((28, 28)).convert('L')
 
-        # 学習時と同じ形に画像データを変換する
-        img_array = np.asarray(img) / 255
-        img_array = img_array.reshape(1, 784)
+def update(pk):
+    """裏側で動いている時間のかかる処理"""
+    progress = get_object_or_404(Progress, pk=pk)
+    for i in range(1, 11):
+        time.sleep(1)
+        progress.num = i * 10  # 初回に10、次に20...最後は100が入る。進捗のパーセントに対応
+        progress.save()
 
-        # 推論した結果を、テンプレートへ渡して表示
-        context = {
-            'result': predict(img_array),
-        }
-        return render(self.request, 'milk/result.html', context)
+
+def progress(request, pk):
+    """現在の進捗ページ"""
+    context = {
+        'progress': get_object_or_404(Progress, pk=pk)
+    }
+    return render(request, 'milk/progress.html', context)
